c35aee83e85e5644093339d77745d5916c8b87b7
marcelosousa <601882+marcelosousa@users.noreply.github.com>
2023-08-24 17:38:54 +0100

    feat: discord poc

---
+++ cli/cmd/flags.go
@@ -0,0 +1,11 @@
+package cmd
+
+var (
+	logLevel string
+	model    string
+)
+
+const (
+	MODEL     = "model"
+	LOG_LEVEL = "log-level"
+)
---
+++ cli/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/trailblazing-reviewpad/chatty/cli/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
